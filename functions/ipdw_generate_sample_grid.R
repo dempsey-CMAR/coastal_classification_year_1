c9be924cdb32ffc5a4519a67f17d4ce8b569d4e9
Danielle Dempsey <ddempsey@perennia.ca>
2024-06-06 09:49:59 -0300

    add option to specify grid size (not just use the mean distance between neighbors)

--- functions/ipdw_generate_sample_grid.R
+++ functions/ipdw_generate_sample_grid.R
@@ -5,6 +5,8 @@
 
 # sf: a spatial data frame of sample locations. Must be in projected coordinates (not lat/long)
 # costras: cost raster generated from ipdw::costrasterGen()
+# base_grid: base grid size in units of the map. Will be multiplied by dist_factor 
+## for final grid size. Default is the mean distance between nearest neighbors.
 # dist_factor: factor controlling the size of the grid cells. Grid size is the mean distance between
 ## neighbors * dist_factor. Default is dist_factor = 2.
 # figures: logical argument. If TRUE, will generate figures of sample_window, point_pattern, and
@@ -17,24 +19,27 @@
 
 ipdw_generate_sample_grid <- function(
     sf, costras,
-    dist_factor = 2, figures = FALSE) {
+    base_grid = NULL,
+    dist_factor = 2) {
 
   # create box around the sample points
   sample_window <- spatstat.geom::owin(
     range(c(st_bbox(sf)["xmin"], st_bbox(sf)["xmax"])),
     range(c(st_bbox(sf)["ymin"], st_bbox(sf)["ymax"]))
   )
-
-  # create point pattern object
-  point_pattern <- spatstat.geom::ppp(
-    st_coordinates(sf)[, 1], st_coordinates(sf)[, 2], window = sample_window
-  )
-
-  # mean distance between neighbors
-  mean_neigh_dist <- mean(nndist(point_pattern))
-
+  
+  if (is.null(base_grid)) {
+    # create point pattern object
+    point_pattern <- spatstat.geom::ppp(
+      st_coordinates(sf)[, 1], st_coordinates(sf)[, 2], window = sample_window
+    )
+    
+    # mean distance between neighbors
+    base_grid <- mean(nndist(point_pattern))
+  }
+  
   # build grid
-  gridsize <- mean_neigh_dist * dist_factor
+  gridsize <- base_grid * dist_factor
   grainscale_factor <- gridsize / res(costras)[1]
 
   # create grid raster
@@ -45,20 +50,20 @@ ipdw_generate_sample_grid <- function(
   # spatial join (assign each station to a grid cell)
   sf_grid <- sf::st_join(sf, st_as_sf(gridpol))
 
-  if(isTRUE(figures)) {
-
-  #  par(mfrow = c(2, 2))
-
-    plot(sample_window)
-    plot(point_pattern)
-
-    plot(costras, main = "sample_grid")
-    lines(gridpol, col = "grey")
-   # points(sf)
-  }
+  # if(isTRUE(figures)) {
+  # 
+  # #  par(mfrow = c(2, 2))
+  # 
+  #   plot(sample_window)
+  #   plot(point_pattern)
+  # 
+  #   plot(costras, main = "sample_grid")
+  #   lines(gridpol, col = "grey")
+  #  # points(sf)
+  # }
 
   list(
-    mean_neigh_dist = mean_neigh_dist,
+    base_grid = base_grid,
     grid = gridpol,
     sf_grid = sf_grid
   )
